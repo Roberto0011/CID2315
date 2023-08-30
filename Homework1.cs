21adaa2200a21c855f924691ff2f386df77e6f27
Roberto0011 <143235012+Roberto0011@users.noreply.github.com>
2023-08-30 11:13:13 -0700

    Add files via upload
    Submitted Homework1.

---
+++ Homework1.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4*X*X+3*Y;
+        /*
+        
+        First shot, dont I just love over complicating things...
+        
+        string xValue = "X = "+ X;
+        string yValue = ", Y = "+ Y;
+        string zValue = "The value of Z is "+Z;
+        Console.Write(xValue);
+        Console.WriteLine(yValue);
+        Console.WriteLine(zValue);*/
+
+        Console.WriteLine($"X = {X}, Y = {Y}");
+        Console.WriteLine($"The value of z is {Z}");
+        
+        
+
+    }
+}
